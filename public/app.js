b81b9518f0ae578e831615c4c6c04ef7b2f80f84
CasperHogstrom <casperhogstrom3@gmail.com>
2022-09-06 13:06:29 +0200

    The JS now creates two div elements with own id's, and two h1 elements

--- public/app.js
+++ public/app.js
@@ -1,1 +1,24 @@
-console.log('Hallo?')
+buildSite();
+
+function buildSite() {
+    
+    const targetSettings = document.getElementById('app');
+    const divsettings = document.createElement('div');
+    divsettings.setAttribute('id', 'settings')
+    targetSettings.appendChild(divsettings);
+
+    const targetCalculator = document.getElementById('app');
+    const divcalculator = document.createElement('div');
+    divcalculator.setAttribute('id', 'calculator');
+    targetCalculator.appendChild(divcalculator);
+
+    const targetdivSettings = document.getElementById('settings');
+    const settingsTitle = document.createElement('h1');
+    settingsTitle.innerHTML = "Settings";
+    targetdivSettings.appendChild(settingsTitle);
+
+    const targetdivCalc = document.getElementById('calculator');
+    const divCalcTitle = document.createElement('h1');
+    divCalcTitle.innerHTML = "Calculator";
+    targetdivCalc.appendChild(divCalcTitle)
+} 
