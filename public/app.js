4634977e89b222a9175cdb2f8daa116e24792daf
CasperHogstrom <casperhogstrom3@gmail.com>
2022-09-08 14:02:41 +0200

    Moved the settings button, added a background color to the settings and calculator div elements, changed the contents of font select element, and added a result header and textarea, which is disabled,

--- public/app.js
+++ public/app.js
@@ -10,50 +10,64 @@ function buildSite() {
     settingsTitle.innerHTML = "Settings";
     settings.appendChild(settingsTitle);
 
-    //Gör knappen, i app div, som ska gömma settings div
-    const HideShowButton = document.createElement('button');
-    HideShowButton.setAttribute('id', 'HSButton')
-    HideShowButton.innerText = 'Settings';
-   //Kolla om settings div är synlig eller ej
-    HideShowButton.onclick = () => settings.hidden = !settings.hidden;
-    app.appendChild(HideShowButton);
-
     const divcalculator = document.createElement('div');
     divcalculator.setAttribute('id', 'calculator');
     app.appendChild(divcalculator);
 
-    const divCalcTitle = document.createElement('h1');
-    divCalcTitle.innerHTML = "Calculator";
-    divcalculator.appendChild(divCalcTitle);
+    //Gör knappen, i calculator div, som ska gömma settings div
+    const HideShowButton = document.createElement('button');
+    HideShowButton.setAttribute('id', 'HSButton')
+    HideShowButton.innerText = 'Settings';
+    //Kolla om settings div är synlig eller ej
+    HideShowButton.onclick = () => settings.hidden = !settings.hidden;
+    calculator.appendChild(HideShowButton);
 
+    const calcTitle = document.createElement('h1');
+    calcTitle.innerHTML = "Calculator";
+    divcalculator.appendChild(calcTitle);
+
+    //Kallar på funktionerna så att de körs
     buildSettings(settings);
-    
     buildCalculator(calculator);
 
 } 
 
 function buildSettings() {
     
+    //Color dropdown
     const slctColor = document.createElement('select');
     slctColor.setAttribute('id', 'color');
     slctColor.setAttribute('name', 'color');
     slctColor.innerText = 'Color';
-    settings.appendChild(slctColor);
 
+    const colorPalevioletred = document.createElement('option');
+    colorPalevioletred.setAttribute('value', 'colorpalevioletred');
+    colorPalevioletred.innerText = 'Palevioletred';
+    slctColor.appendChild(colorPalevioletred);
+
+    const colorCoral = document.createElement('option');
+    colorCoral.setAttribute('value', 'colorcoral')
+    colorCoral.innerText = 'Coral';
+    slctColor.appendChild(colorCoral);
+
+    //Font dropdown
     const slctFont = document.createElement('select');
     slctFont.setAttribute('id', 'font');
     slctFont.setAttribute('name', 'font');
     slctFont.innerText = 'font';
 
-    const ColorCoral = document.createElement('option');
-    ColorCoral.setAttribute('value', 'colorcoral')
-    ColorCoral.innerText = 'Coral';
-    slctColor.appendChild(ColorCoral);
+    const fontSize12 = document.createElement('option');
+    fontSize12.setAttribute('value', 'fontsize12')
+    fontSize12.innerText = 'Font size 12px';
+    slctFont.appendChild(fontSize12);
 
-    const ColorPalevioletred = document.createElement('option');
-    ColorPalevioletred.setAttribute('value', 'colorpalevioletred');
-    slctColor.appendChild(ColorPalevioletred);
+    const fontSize14 = document.createElement('option');
+    fontSize14.setAttribute('value', 'fontsize14');
+    fontSize14.innerText = 'Font size 14px';
+    slctFont.appendChild(fontSize14);
 
+    //skriv ut select elementen
+    settings.appendChild(slctColor)
     settings.appendChild(slctFont);
 
 
@@ -61,28 +75,33 @@ function buildSettings() {
 
 function buildCalculator() {    
 
+    //Input element för första nr
     const numberInput1 = document.createElement('input');
     numberInput1.setAttribute('id', 'number1');
     calculator.appendChild(numberInput1);
 
+    //Button element för addition
     const btnAddition = document.createElement('button');
     btnAddition.setAttribute('id', 'addition');
     btnAddition.setAttribute('name', 'addition');
     btnAddition.innerText = '+';
     calculator.appendChild(btnAddition);
 
+    //Button element för subtraktion
     const btnSubtraction = document.createElement('button');
     btnSubtraction.setAttribute('id', 'subtraction');
     btnSubtraction.setAttribute('name', 'subtraction');
     btnSubtraction.innerText = '-';
     calculator.appendChild(btnSubtraction);
 
+    //Button element för multiplikation
     const btnMultiplication = document.createElement('button');
     btnMultiplication.setAttribute('id', 'multiplication');
     btnMultiplication.setAttribute('name', 'multiplication');
     btnMultiplication.innerText = '*';
     calculator.appendChild(btnMultiplication);
 
+    //Button element för division
     const btnDivision = document.createElement('button');
     btnDivision.setAttribute('id', 'division');
     btnDivision.setAttribute('name', 'division');
@@ -93,6 +112,15 @@ function buildCalculator() {
     numberInput2.setAttribute('id', 'number2');
     calculator.appendChild(numberInput2);
 
-    
+    //Input element för andra nr
+    const resultTitle = document.createElement('h2');
+    resultTitle.innerText = 'Result';
+    calculator.appendChild(resultTitle);
+
+    //Textarea element för svaret
+    const resultBox = document.createElement('textarea');
+    resultBox.setAttribute('id', 'results');
+    resultBox.disabled = true;
+    calculator.appendChild(resultBox);
 }
 
