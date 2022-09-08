e67b13c7507d2b279e8a0378490a6e2772890a05
CasperHogstrom <casperhogstrom3@gmail.com>
2022-09-08 16:00:21 +0200

    The calculator is now fully operational, added a clear button, and the maths buttons now work, also moved the maths buttons

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@ function buildSite() {
     app.appendChild(divsettings);
 
     const settingsTitle = document.createElement('h1');
-    settingsTitle.setAttribute('id', 'settingtitle')
+    settingsTitle.setAttribute('id', 'settingtitle');
     settingsTitle.innerHTML = "Settings";
     settings.appendChild(settingsTitle);
 
@@ -48,7 +48,7 @@ function buildSettings() {
     slctColor.appendChild(colorPalevioletred);
 
     const colorDarkgrey = document.createElement('option');
-    colorDarkgrey.setAttribute('value', 'darkgrey')
+    colorDarkgrey.setAttribute('value', 'darkgrey');
     colorDarkgrey.innerText = 'Darkgrey';
     slctColor.onchange = (event) => {
         
@@ -70,45 +70,48 @@ function buildSettings() {
     slctFont.innerText = 'font';
 
     const fontSize12 = document.createElement('option');
-    fontSize12.setAttribute('value', 'fontsize12')
+    fontSize12.setAttribute('value', 'fontsize12');
     fontSize12.innerText = 'Font size 12px';
     slctFont.appendChild(fontSize12);
 
-    const fontSize14 = document.createElement('option');
-    fontSize14.setAttribute('value', 'fontsize14');
-    fontSize14.innerText = 'Font size 14px';
+    const fontSize16 = document.createElement('option');
+    fontSize16.setAttribute('value', 'fontsize16');
+    fontSize16.innerText = 'Font size 16px';
     slctFont.onchange = (event) => {
         
-        calculator.classList.remove('fontsize12');
-        calculator.classList.remove('fontsize14');
-        settings.classList.remove('fontsize12'); 
-        settings.classList.remove('fontsize14'); 
-
-        calculator.classList.add(event.target.value);
-        settings.classList.add(event.target.value);
+        results.className = event.target.value;
 
     };
-    slctFont.appendChild(fontSize14);
+    slctFont.appendChild(fontSize16);
 
     //skriv ut select elementen
-    settings.appendChild(slctColor)
+    settings.appendChild(slctColor);
     settings.appendChild(slctFont);
 
 
 }
 
-function buildCalculator() {    
+function buildCalculator(results) {    
 
     //Input element för första nr
     const numberInput1 = document.createElement('input');
     numberInput1.setAttribute('id', 'number1');
     calculator.appendChild(numberInput1);
 
+    //Input element för andra nr
+    const numberInput2 = document.createElement('input');
+    numberInput2.setAttribute('id', 'number2');
+    calculator.appendChild(numberInput2);
+
     //Button element för addition
     const btnAddition = document.createElement('button');
     btnAddition.setAttribute('id', 'addition');
     btnAddition.setAttribute('name', 'addition');
     btnAddition.innerText = '+';
+    btnAddition.onclick = () => {
+        resultBox.append(numberInput1.value + " + " + numberInput2.value + " = " +
+        (Number(numberInput1.value) + Number(numberInput2.value)) + "\n");
+    }
     calculator.appendChild(btnAddition);
 
     //Button element för subtraktion
@@ -116,6 +119,10 @@ function buildCalculator() {
     btnSubtraction.setAttribute('id', 'subtraction');
     btnSubtraction.setAttribute('name', 'subtraction');
     btnSubtraction.innerText = '-';
+    btnSubtraction.onclick = () => {
+        resultBox.append(numberInput1.value + " - " + numberInput2.value + " = " +
+        (Number(numberInput1.value) - Number(numberInput2.value)) + "\n");
+    }
     calculator.appendChild(btnSubtraction);
 
     //Button element för multiplikation
@@ -123,6 +130,10 @@ function buildCalculator() {
     btnMultiplication.setAttribute('id', 'multiplication');
     btnMultiplication.setAttribute('name', 'multiplication');
     btnMultiplication.innerText = '*';
+    btnMultiplication.onclick = () => {
+        resultBox.append(numberInput1.value + " * " + numberInput2.value + " = " +
+        (Number(numberInput1.value) * Number(numberInput2.value)) + "\n");
+    }
     calculator.appendChild(btnMultiplication);
 
     //Button element för division
@@ -130,13 +141,12 @@ function buildCalculator() {
     btnDivision.setAttribute('id', 'division');
     btnDivision.setAttribute('name', 'division');
     btnDivision.innerText = '/';
+    btnDivision.onclick = () => {
+        resultBox.append(numberInput1.value + " / " + numberInput2.value + " = " +
+        (Number(numberInput1.value) / Number(numberInput2.value)) + "\n");
+    }
     calculator.appendChild(btnDivision);
 
-    const numberInput2 = document.createElement('input');
-    numberInput2.setAttribute('id', 'number2');
-    calculator.appendChild(numberInput2);
-
-    //Input element för andra nr
     const resultTitle = document.createElement('h2');
     resultTitle.innerText = 'Result';
     calculator.appendChild(resultTitle);
@@ -146,5 +156,13 @@ function buildCalculator() {
     resultBox.setAttribute('id', 'results');
     resultBox.disabled = true;
     calculator.appendChild(resultBox);
+
+    const clearBtn = document.createElement('button')
+    clearBtn.setAttribute('id', 'clearbutton');
+    clearBtn.setAttribute('name', 'clearbutton');
+    clearBtn.innerText = 'Clear';
+    clearBtn.onclick = () => resultBox.innerText ='';
+    calculator.appendChild(clearBtn);
+
 }
 
